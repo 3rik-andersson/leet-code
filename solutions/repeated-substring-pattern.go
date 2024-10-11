dc1cb62e6d0470ffb8af6e5a4c54edadcc57b3c2
Erik Andersson <erik0304andersson@gmail.com>
2024-10-11 13:47:50 +0200

    feat: add repeated string solution

---
+++ solutions/repeated-substring-pattern.go
@@ -0,0 +1,19 @@
+package solutions
+
+import "strings"
+
+func RepeatedSubstringPattern(s string) bool {
+	n := len(s)
+	for i := 1; i <= n/2; i++ {
+		if n%i == 0 {
+			builder := strings.Builder{}
+			for j := 0; j < n/i; j++ {
+				builder.WriteString(s[:i])
+			}
+			if builder.String() == s {
+				return true
+			}
+		}
+	}
+	return false
+}
